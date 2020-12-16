d0e74b0c08c7b07cf60fc12024b3993ed87921dd
aadhityasw <aadhityas@gmail.com>
2020-12-16 16:39:33 +0530

    modifications in ruby project 1

--- ruby/demo_blog/app/controllers/posts_controller.rb
+++ ruby/demo_blog/app/controllers/posts_controller.rb
@@ -1,13 +1,42 @@
 class PostsController < ApplicationController
+
+  before_action :current_post, only: [:show, :edit, :update, :destroy]
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.create(post_params)
+    redirect_to post_path(post)
   end
 
   def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to post_path
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:author, :content)
+  end
+
+  def current_post
+    @post = Post.find(params[:id])
   end
 end
